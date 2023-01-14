9130e73656ae2ceddd78fb40e39e0bd393e22f6a
Stefan-Seidel-AT <97153913+Stefan-Seidel-AT@users.noreply.github.com>
2023-01-14 21:32:45 +0100

    Update 01_start_R_tensorflow.sh
    remove and format code

--- 01_start_R_tensorflow.sh
+++ 01_start_R_tensorflow.sh
@@ -4,13 +4,5 @@
 echo "start tensorflow :-) "
 echo "$PWD"
 
-### To start Seurat and mount current directory
-#echo "docker run -it -v "$PWD":/work -p 8080:8080 satijalab/seurat:latest"
-#docker run -it -v $PWD:/work -p 8080:8080 satijalab/seurat:latest
-#echo " to start \"tensorflow\" insert: localhost:8787 to the browser "
-
-#echo "docker run -v "$PWD":/work -p 8787:8787 -e DISABLE_AUTH=true 0f56500683f2 jupyter/tensorflow-notebook"
 echo "docker run -v \"$PWD\":/home/jovyan/work -p 8888:8888 jupyter/tensorflow-notebook"
 docker run -v "$PWD":/home/jovyan/work -p 8888:8888 jupyter/tensorflow-notebook
-#docker run -v "$PWD":/work -p 8888:8888 jupyter/tensorflow-notebook
-
